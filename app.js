cf2febbbd52ac541ea735464ba40a9226010ce09
SCarmo <scarmody@tcd.ie>
2017-12-11 16:23:09 +0000

    Can now read Json from API & graph it

--- app.js
+++ app.js
@@ -1,53 +1,88 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const github = require('octonode');
-const path = require('path');
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const client = github.client();
 const fetch = require('node-fetch');
-const file = 'views/flare.csv';
+const file = 'views/graphData.csv';
 const fs = require('fs');
 const csv = require('fast-csv');
+var EA;
+var request = require('request');
 
-var obj = require('./views/user.json');
-console.log(obj.login);
+fs.truncate(file, 0, function(){})
 
-fs.readFile('views/followers.json', 'utf8', function (err,data) {
-    data = JSON.parse(data);
-    console.log(data.length);
-    for(var i = 0; i < data.length; i++) {
-        console.log(data[i].login);
-    }
-});
-
-fs.truncate(file, 0, function(){console.log('csv file created')})
 fs.appendFile(file, 'id, value\n', function (err) {
    if (err) throw err;
  });
-// fs.appendFile(file, 'flare.vis.Visualization,16540\n', function (err) {
-//   if (err) throw err;
-// });
 
- fs.appendFile(file, 'flare.vis.Visualization,16540\n', function (err) {
-   if (err) throw err;
+var options = {
+   url: "https://api.github.com/orgs/electronicarts/repos",
+   headers: {
+     'User-Agent': 'request'
+   }
+ };
+          /* For reading json data from api*/
+request(options,"https://api.github.com/orgs/electronicarts/repos", function (error, response, body) {
+     parse(body);
  });
 
-// fs.createReadStream(file)
-//   .pipe(csv())
-//   .on('data', function(data){
-//     console.log(data);
-//   })
-//   .on('end', function(data){
-//     console.log('finished!');
-//   })
+function parse(body){
+  var repos = JSON.parse(body);
+  var keys;
+  var str;
+  fs.appendFile(file, "EA,\n", function (err) {
+    if (err) throw err;
+  });
 
-// fetch('https://api.github.com/users/scarmo')
-//     .then(function(res) {
-//         return res.json();
-//     }).then(function(json) {
-//         console.log(json);
-//     });
+  // for every repo
+  for(var i = 0; i < repos.length; i++) {
+    str = "EA." + repos[i].name + ",\n";
+    fs.appendFile(file, str, function (err) {
+      if (err) throw err;
+    });
+    keys = Object.keys(repos[i]);
+    var string = JSON.stringify(repos[i]);
+    var val = JSON.parse(string);
+
+    // for elements in a repo
+    for(var k = 0; k < 20; k++){
+      str = "EA." + repos[i].name + "." + keys[k] + ",\n";
+      fs.appendFile(file, str, function (err) {
+        if (err) throw err;
+      });
+      str =  "EA." + repos[i].name + "." + keys[k] + "." +val[keys[k]] + ",1\n";
+      fs.appendFile(file, str, function (err) {
+        if (err) throw err;
+      });
+    }
+  }
+}
+
+        /* when reading json data from file*/
+//  fs.readFile('views/org.json', 'utf8', function (err,data) {
+    // var repos = JSON.parse(EA);
+    // var keys;
+    // var str;
+    // // for every repo
+    // for(var i = 0; i < repos.length; i++) {
+    //   str = "EA." + repos[i].name + ",\n";
+    //   fs.appendFile(file, str, function (err) {
+    //     if (err) throw err;
+    //   });
+    //   keys = Object.keys(repos[i]);
+    //   var string = JSON.stringify(repos[i]);
+    //   var val = JSON.parse(string);
+    //   // for elements in a repo
+    //   for(var k = 0; k < 5; k++){
+    //     str = "EA." + repos[i].name + "." + keys[k] + ",\n";
+    //     fs.appendFile(file, str, function (err) {
+    //       if (err) throw err;
+    //     });
+    //     str =  "EA." + repos[i].name + "." + keys[k] + "." +val[keys[k]] + ",1\n";
+    //     fs.appendFile(file, str, function (err) {
+    //       if (err) throw err;
+    //     });
+    //   }
+    // }
+//});
 
 app.set('view engine', 'ejs');
 
